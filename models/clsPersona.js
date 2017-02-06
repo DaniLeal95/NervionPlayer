1fd555b3742208a423d7c8d11c1fa3e13121fe87
Daniel Leal Reyes <dlealreyes@gmail.com>
2017-02-06 22:53:07 +0100

    Inicio clases

---
+++ models/clsPersona.js
@@ -0,0 +1,33 @@
+class Persona{
+	
+	var id;
+    var nombre;
+    var apellidos;
+    var contraseña;
+    var alias;
+    var correo;
+    var fecha_Creacion;
+    var curso;
+    var letra;
+    var observaciones;
+    var confirmado;
+    var foto;
+
+    constructor(id,nombre,apellidos,contraseña,alias,correo,fecha_Creacion,curso,letra,observaciones,confirmado,foto){
+    	this.id= id;
+    	this.nombre= nombre;
+    	this.apellidos = apellidos;
+    	this.contraseña = contraseña;
+    	this.alias = alias;
+    	this.correo = correo;
+    	this.fecha_Creacion = fecha_Creacion;
+    	this.curso = curso;
+    	this.letra = letra;
+    	this.observaciones = observaciones;
+    	this.confirmado  = confirmado ;
+    	this.foto = foto;
+    }
+    
+
+
+}
